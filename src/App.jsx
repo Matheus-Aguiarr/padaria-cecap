a20c0a8069aeeff8dd1b30ffd2b6e70571eaa85e
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2025-03-05 11:57:48 -0300

    Adicionei componente About

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     navigate("/menu");
   }
   return (
-    <div className="w-[100vw]">
+    <div className="w-[100vw] bg-[#c8eaff]">
       <Header />
       {/* Home comeca aqui */}
       <Home />
@@ -22,12 +22,12 @@ function App() {
         <h2 className="text-center font-bold text-3xl overflow-y-hidden">
           Nosso <span className="text-[#126091]">Cardápio</span>
         </h2>
-        <div className="menu-preview flex  gap-8">
+        <div className="menu-preview flex gap-8">
           <Itens nome="Pão Francês fresquinho" imagem={imgHome} />
           <Itens nome="Cafezin fresquin" imagem={imgHome} />
         </div>
 
-        <div className="menu-way" onClick={onSeeMenuClick}>
+        <div className="menu-way overflow-y-hidden" onClick={onSeeMenuClick}>
           <a className="more bg-[#126091] text-white rounded-lg text-center hover:bg-white hover:text-[#126091] hover:border-[#126091] border-2 border-[#126091] duration-500 hover:shadow-lg shadow-[#126091] px-4 py-2 text-[20px]">
             Confira o <span className="">Cardapio Completo</span>
           </a>
---
+++ src/components/About/About.jsx
@@ -0,0 +1,7 @@
+import "./About.css";
+
+const About = () => {
+  return <div>Hello, World</div>;
+};
+
+export default About;
