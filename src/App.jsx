644094e105e11edcc199c9ef275d3a089d4d3a3c
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2025-03-09 14:28:49 -0300

    Resolvendo problema de caminho no import do MenuWsy

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import Address from "./components/Address/Address.jsx";
-import MenuWay from ".components/MenuWay/MenuWay.jsx";
+import MenuWay from "./components/MenuWay/MenuWay.jsx";
 
 
 function App() {
-
   return (
     <div className="w-[100vw] h-[100vh] bg-[#c8eaff]">
       <Header />
